9001297f9d9b8e243877a6a973be50d8b0af9828
p@ssw0rd <keviny0un9@hotmail.com>
2015-06-13 21:28:10 +0800

    init commit

---
+++ Git-Study/src/com/kevin/test/GitTest.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:Git-Study
+ * File Name:GitTest.java
+ * Package Name:com.kevin.test
+ * Date:2015��6��13������9:12:49
+ * Copyright (c) 2015, chenzhou1025@126.com All Rights Reserved.
+ *
+*/
+
+package com.kevin.test;
+/**
+ * ClassName: GitTest <br/>
+ * Function: Test GitHub. <br/>
+ * Reason: Test GitHub. <br/>
+ * date: 2015年6月13日 下午9:26:02 <br/>
+ *
+ * @author Kevin Yang
+ * @version 
+ * @since JDK 1.6
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("git init");
+	}
+}
+
